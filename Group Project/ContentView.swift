f8e20fba95b422aef48a84f305e90ca281dfb9f0
Roopeksha Agrawal <helloimapotatoe90@gmail.com>
2023-06-12 10:52:23 -0500

    dalkjflkajl

--- Group Project/ContentView.swift
+++ Group Project/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, testing...")
+        Text("Hello, testing github changes and stuff idekatp...")
             .padding()
     }
 }
