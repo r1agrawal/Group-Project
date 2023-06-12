c7d568099cdd2a9e252b330c7c1905d5674df6fc
Roopeksha Agrawal <helloimapotatoe90@gmail.com>
2023-06-12 10:39:47 -0500

    Initial Commit

---
+++ Group Project/Group_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group_ProjectApp.swift
+//  Group Project
+//
+//  Created by User on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Group_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
