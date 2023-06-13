c21c7c73cf7d0bed4d1a3580d7c4226eba2208e5
Roopeksha Agrawal <helloimapotatoe90@gmail.com>
2023-06-13 14:08:29 -0500

    Adding files and stuff idrk

--- Group Project/ContentView.swift
+++ Group Project/ContentView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, testing github changes and stuff idekatp...")
-            .padding()
-        Text("hello")
-        Text("Bye")
-        Text("helloooo")
-        Text("byeeee")
-        Text("Goodbyeeeee")
+        Text("")
     }
 }
 
---
+++ Group Project/Gallery.swift
@@ -0,0 +1,20 @@
+//
+//  Gallery.swift
+//  Group Project
+//
+//  Created by User on 6/13/23.
+//
+
+import SwiftUI
+
+struct Gallery: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Gallery_Previews: PreviewProvider {
+    static var previews: some View {
+        Gallery()
+    }
+}
